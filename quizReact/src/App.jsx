330a5d097e49fb2964a3e06d9cc220c83e5b8d4e
Allanpt <jatallantavares@gmail.com>
2023-10-23 05:49:01 -0300

    inicial tools

---
+++ quizReact/src/App.jsx
@@ -0,0 +1,17 @@
+// react, componentes, estáticos(css)
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Quiz de programação</h1>
+      <Welcome/>
+    </>
+  )
+}
+
+export default App
